4497a75e24edf4efd6b410852fd28fe46edb5d10
SaúlTejeda117 <79871748+saulTejeda117@users.noreply.github.com>
2022-08-14 20:27:39 -0500

    Update numericPuzzle.py

--- numericPuzzle.py
+++ numericPuzzle.py
@@ -1,24 +1,31 @@
 # Numeric Puzzle
+
+# GUI dependencies
 from tkinter import *
 import tkinter
 import tkinter as tk
 from tkinter import messagebox
 
+# file exists validation 
 import os
 
 import random
 
+# count seconds dependencies
 from datetime import datetime
 
 def main():
+    # principal interface features
     mainWindow = tk.Tk()
     mainWindow.title('Numeric Puzzle')
     mainWindow.config(width = 380, height=380)
     mainWindow.resizable(False, False)
-    print(os.getcwd())
+
+    # read the file with the best time 
     with open("RecordsList.txt") as f: 
         lines = f.readlines()
 
+    # format time /minute:seconds/
     def format_time(seconds):
         hour = int(seconds / 60 / 60)
         seconds -= hour*60*60
@@ -33,9 +40,11 @@ def main():
         actual_time = format_time(int(currentSeconds))
         tk.Label(text = 'Time: ' + str(actual_time), font = ('Arial', 15), fg='#7F8C8D').place(x=5, y= 5)
         mainWindow.after(500, refresh_time)
-
+    
+    # check when 
     def validate_endposition():
         correctPos = 0
+        # where buttons are locate
         positionList = ['20,60','110,60','200,60', '290,60','20,140','110,140','200,140', '290,140','20,220','110,220','200,220', '290,220','20,300','110,300','200,300', '290,300'] 
         i = 0
         num = 1
@@ -56,6 +65,8 @@ def main():
         if(correctPos>=15):
             print(str(actual_time))
             fileExists = os.path.exists('RecordsList.txt')
+            # check if records file txt exists
+            # write a new record 
             if (fileExists == True):
                 with open("RecordsList.txt", "w+") as f: f.write(str(actual_time)+'\n')
             else:
@@ -63,11 +74,12 @@ def main():
             f.close()
             mainWindow.destroy()
             main()
-            quit()               
+            quit()    
 
+    # starting the game
     def shuffle_blocks(botonStart, botonScores):
-        # List screen elements position
         global positionList
+        # List screen elements position
         positionList = ['20,60','110,60','200,60', '290,60','20,140','110,140','200,140', '290,140','20,220','110,220','200,220', '290,220','20,300','110,300','200,300', '290,300']
         botonStart = mainWindow.nametowidget(botonStart)
         botonScores = mainWindow.nametowidget(botonScores)
@@ -76,9 +88,9 @@ def main():
         botonStart.place_forget()
         botonScores.place_forget()
         newPositionList = [None]*16
-        # Start the time count
         global begin 
         global actual_time
+        # Start the time count
         begin = datetime.now()
         refresh_time()
         num = 1
@@ -88,7 +100,6 @@ def main():
             botonName = mainWindow.nametowidget('boton' + str(num))
             a = botonName.place_info()
             newPositionList
-            # botonName.place_forget()
             newPosition = random.randint(0,15)
             if(positionList[newPosition] != None):
                 position = positionList[newPosition]
@@ -129,31 +140,36 @@ def main():
         emptyButtonYPosition = int(emptyButtonInfo['y'])
         # Movement Validation
         if(emptyButtonXPosition == clickedButtonXPosition):
+            # if a button can move to up position
             if(emptyButtonYPosition + 80 == clickedButtonYPosition):
                 emptyButtonName.place_forget()
                 clickedButtonName.place_forget()
-                emptyButtonName.place(x=clickedButtonXPosition, y=clickedButtonYPosition)
-                clickedButtonName.place(x=emptyButtonXPosition, y=emptyButtonYPosition)
+                emptyButtonName.place(x = clickedButtonXPosition, y = clickedButtonYPosition)
+                clickedButtonName.place(x = emptyButtonXPosition, y = emptyButtonYPosition)
+            # if a button can move to down position
             elif(emptyButtonYPosition - 80 == clickedButtonYPosition):
                 emptyButtonName.place_forget()
                 clickedButtonName.place_forget()
-                emptyButtonName.place(x=clickedButtonXPosition, y=clickedButtonYPosition)
-                clickedButtonName.place(x=emptyButtonXPosition, y=emptyButtonYPosition)
+                emptyButtonName.place(x = clickedButtonXPosition, y = clickedButtonYPosition)
+                clickedButtonName.place(x = emptyButtonXPosition, y = emptyButtonYPosition)
+            # if a button can move to left position
         elif(emptyButtonYPosition == clickedButtonYPosition):
             if(emptyButtonXPosition - 90 == clickedButtonXPosition):
                 emptyButtonName.place_forget()
                 clickedButtonName.place_forget()
-                emptyButtonName.place(x=clickedButtonXPosition, y=clickedButtonYPosition)
-                clickedButtonName.place(x=emptyButtonXPosition, y=emptyButtonYPosition)
+                emptyButtonName.place(x = clickedButtonXPosition, y = clickedButtonYPosition)
+                clickedButtonName.place(x = emptyButtonXPosition, y = emptyButtonYPosition)
+            # if a button can move to right position
             if(emptyButtonXPosition + 90 == clickedButtonXPosition):
                 emptyButtonName.place_forget()
                 clickedButtonName.place_forget()
-                emptyButtonName.place(x=clickedButtonXPosition, y=clickedButtonYPosition)
-                clickedButtonName.place(x=emptyButtonXPosition, y=emptyButtonYPosition)
+                emptyButtonName.place(x = clickedButtonXPosition, y = clickedButtonYPosition)
+                clickedButtonName.place(x = emptyButtonXPosition, y = emptyButtonYPosition)
+    # Title of th screen
+    tk.Button(mainWindow, width = 54,height = 23, bg='#B2BABB', state = 'disabled', bd = 0).place(x = 0, y = 40)
+    windowTitle = tk.Label(text = 'Numeric Puzzle', font = ('Arial', 15), fg = '#7F8C8D')
+    windowTitle.place(x = 5, y= 5)
     # Inicial position of the buttons
-    tk.Button(mainWindow, width=54,height=23, bg='#B2BABB', state='disabled', bd=0).place(x = 0, y = 40)
-    windowTitle = tk.Label(text = 'Numeric Puzzle', font = ('Arial', 15), fg='#7F8C8D')
-    windowTitle.place(x=5, y= 5)
     xpos = 20 
     ypos = 60
     i = 1
@@ -167,11 +183,11 @@ def main():
                 tk.Button(mainWindow, text = i, name = 'boton' + str(i), width=10, height=4, bd = 0, bg='#ffffff', command = lambda clickedButton = i: move_block(clickedButton), state='disabled', font=('Helvetica', 9)).place(x = xpos, y = ypos)
             xpos += 90
             i += 1
-            j+=1
+            j += 1
         xpos = 20
         ypos += 80
         j = 1 
-    tk.Button(mainWindow, text = 'Best Scores:' + '\n' + str(lines), name= 'botonScores', width=25, height=10, anchor=tkinter.N, state='disabled').place(relx=.5, rely=.5, anchor=CENTER)
-    tk.Button(mainWindow, text = 'Start the Game!', name= 'botonStart', width=15, height=2, command = lambda botonStart = 'botonStart', botonScores = 'botonScores': shuffle_blocks(botonStart, botonScores)).place(relx=.5, rely=.6,anchor= CENTER)
+    tk.Button(mainWindow, text = 'Best Scores:' + '\n' + str(lines), name = 'botonScores', width = 25, height=10, anchor = tkinter.N, state='disabled').place(relx = .5, rely=.5, anchor = CENTER)
+    tk.Button(mainWindow, text = 'Start the Game!', name = 'botonStart', width = 15, height = 2, command = lambda botonStart = 'botonStart', botonScores = 'botonScores': shuffle_blocks(botonStart, botonScores)).place(relx = .5, rely = .6,anchor = CENTER)
     mainWindow.mainloop()
 main()
